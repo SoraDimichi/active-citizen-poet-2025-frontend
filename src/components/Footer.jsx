e5611b08615490b9b585453b0fb1c396d9b3a7ab
Alexey Kovalev <Alexey.Kovalev@kofax.com>
2020-12-26 13:34:53 +0300

    Feature: Telephone and letter icon are added to the project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import telephoneLogo from '../images/telephone_icon.svg';
+import letterLogo from '../images/letter_icon.svg';
 
 function Footer() {
   return (
@@ -7,8 +9,26 @@ function Footer() {
         <p className="footer__copyright">&copy; 2020 Local Help</p>
         <nav>
           <ul className="footer__links">
-            <li><a href="http://google.com" className=" footer__link">Телефон</a></li>
-            <li><a href="http://google.com" className="footer__link">Почта</a></li>
+            <li>
+              <a href="http://google.com" className="footer__link">
+                <img
+                  className="footer__logo"
+                  src={telephoneLogo}
+                  alt="логотип телефона"
+                />
+                +7 446 666 00 11
+              </a>
+            </li>
+            <li>
+              <a href="http://google.com" className="footer__link">
+                <img
+                  className="footer__logo"
+                  src={letterLogo}
+                  alt="логотип телефона"
+                />
+                yandex@yandex.ru
+              </a>
+            </li>
           </ul>
         </nav>
       </div>
